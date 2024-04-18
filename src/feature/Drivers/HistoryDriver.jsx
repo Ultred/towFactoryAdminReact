1c6f81de363650bc252f907f9207da85d6841dca
Vincent V <vincentvinuya33@gmail.com>
2024-04-18 10:58:48 +0800

    updateHistoryDriver

--- src/feature/Drivers/HistoryDriver.jsx
+++ src/feature/Drivers/HistoryDriver.jsx
@@ -1,26 +1,56 @@
 import Avatar from "../../components/Avatar";
+import { FaCalendarDays } from "react-icons/fa6";
 import { driverData } from "../../utils/DataSample";
 import styles from "./HistoryDriver.module.css";
+import SortComponent from "../../components/SortComponent";
 
 const HistoryDriver = () => {
   return (
     <div className={styles.historyDriverContainer}>
       <div className={styles.historyDriveCont1}>
-        <h2 className={styles.texth2Style}>Drivers Name</h2>
-        <div>
+        <h2 className={styles.texth2Large}>Drivers Name</h2>
+        <div className={styles.driverContData}>
           {driverData.map((data) => (
             <div className={styles.historyDriveFlex} key={data.id}>
               <Avatar status={data.status} />
-              <div>
-                <p>{data.name}</p>
-                <p>{data.status}</p>
+              <div className={styles.flexDirection}>
+                <p className={styles.texth2Style}>{data.name}</p>
+                <p className={styles.texth2Stylep}>{data.status}</p>
               </div>
             </div>
           ))}
         </div>
       </div>
       <div className={styles.historyDriveCont2}>
-        <div></div>
+        <div className={styles.flexHistoryDriveCont2}>
+          <SortComponent />
+          <button className={styles.flexCalendar}>
+            <FaCalendarDays /> Calendar
+          </button>
+        </div>
+        <div className={styles.contHistoryDriverMain}>
+          <div className={styles.contDriverTop}>
+            <span>Icon </span>
+            <img src="/src/assets/driverPic1.svg" alt="profileDriver" />
+          </div>
+          <div className={styles.textMainCenter}>
+            <h2 className={styles.texth2Large}>John Batumbakal</h2>
+
+            <p className={styles.textBlue}>TOTAL OF TRIPS: 2</p>
+            <div className={styles.textLeft}>
+              <p>AD-ONS (SPECIAL TOOLS)</p>
+              <ul>
+                <li>ROLLER </li>
+                <li>BATTERY CHARGER </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.textMainBottom}>
+            <p>
+              Total amount : <span>1000</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
